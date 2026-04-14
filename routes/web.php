e81abb657dbd0f4efe1491cc47dc4beceebff71f
thuthao211 <030239230218@st.buh.edu.vn>
2026-04-14 09:16:55 +0700

    câu 5, 6

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller5;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -9,6 +10,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+Route::get('/timkiem', [Controller5::class, 'timkiem'])->name('laptop.timkiem');
 
 require __DIR__.'/auth.php';
